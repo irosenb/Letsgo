f54be55eeaa26a26dc5faa3d4c31f40c1120ff64
Colin Zhang <Colin.Zhang@comayers.com>
2013-10-19 20:16:44 -0400

    added basic login page

---
+++ views/login.php
@@ -0,0 +1,7 @@
+<div id="loginform">
+	<form action="" method="POST">
+		<label for="username"></label><input id="username" type="text" name="username" placeholder="Username"><br />
+		<label for="password"></label><input id="password" type="password" name="password" placeholder="Password"><br />
+		<input type="submit" value="Login"><br />
+	</form>
+</div>
