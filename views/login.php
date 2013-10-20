7caeebfdcdd06764482362d09e88754c5101e1dd
Derek Jobst <derekjobst@gmail.com>
2013-10-20 12:15:22 -0400

    Login CSS

--- views/login.php
+++ views/login.php
@@ -5,8 +5,8 @@ Welcome to Let's Go! The place to find new friends who like to go to the same ev
 </div>
 
 <div class="toggle-wrapper">
-	<input type="button" class="button" id="login-toggle" value="Login">
-	<input type="button" class="button" id="register-toggle" value="Sign Up">
+	<input type="button" class="toggle-button" id="login-toggle" value="Login">
+	<input type="button" class="toggle-button" id="register-toggle" value="Sign Up">
 </div>
 
 <div class="login-wrapper">
@@ -15,7 +15,6 @@ Welcome to Let's Go! The place to find new friends who like to go to the same ev
 			<input class="login-field" id="email" type="text" name="email" placeholder="Email address"><br />
 			<input class="login-field" id="password" type="password" name="password" placeholder="Password"><br />
 			<input type="submit" name="login" class="button login-button" value="Login"><br/>
-<!-- 			<p><a href="#" id="registerlink">Create an account</a></p> -->
 		</form>
 	</div>
 	<div id="registerform">
@@ -25,7 +24,6 @@ Welcome to Let's Go! The place to find new friends who like to go to the same ev
 			<input class="login-field" id="name" type="text" name="name" placeholder="Your name"><br />
 			<input class="login-field" id="phone" type="phone" name="phone" placeholder="Phone number"><br />
 			<input type="submit" name="register" class="button login-button" value="Register"><br/>
-<!-- 			<p><a href="#" id="loginlink">Login with existing account</a></p> -->
 		</form>
 	</div>
 </div>
