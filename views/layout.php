43487e1332dea63e5964482440421d0c70055566
Isaac Rosenberg <irosenb7@gmail.com>
2013-10-19 22:41:59 -0400

    added jquery autocomplete

--- views/layout.php
+++ views/layout.php
@@ -12,28 +12,28 @@
 	<title>Let's Go!</title>
 	<link href="style-layout.css" rel="stylesheet" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400|Kaushan+Script' rel='stylesheet' type='text/css'>
-
 </head>
 
 <body>
-<div class="background">
+	<div class="background">
 
-	<div class="body-wrapper">
-	
-		<div class="header-wrapper">
-			<div class="header-text">
-				Let's Go!
+		<div class="body-wrapper">
+		
+			<div class="header-wrapper">
+				<div class="header-text">
+					Let's Go!
+				</div>
 			</div>
+
+
+			
+			</div>
+
+			<div class="footer-wrapper"></div>
+			
 		</div>
-
-
-		
-		</div>
-
-		<div class="footer-wrapper"></div>
-		
 	</div>
-</div>
+<script src="resources/select2.js"></script>
 </body>
 
 </html>
