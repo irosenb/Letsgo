0bb57fe78b4dfe456fc451b5f9990c97d84bfcf2
Derek Jobst <derekjobst@gmail.com>
2013-10-19 21:45:05 -0400

    Updated Style Sheets and Layout File

--- views/layout.php
+++ views/layout.php
@@ -1,3 +1,39 @@
 <?php echo $content ?>
 
 <!DOCTYPE html>
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	
+	<title>Let's Go!</title>
+	<link href="style-layout.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400|Kaushan+Script' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+<div class="background">
+
+	<div class="body-wrapper">
+	
+		<div class="header-wrapper">
+			<div class="header-text">
+				Let's Go!
+			</div>
+		</div>
+
+
+		
+		</div>
+
+		<div class="footer-wrapper"></div>
+		
+	</div>
+</div>
+</body>
+
+</html>
