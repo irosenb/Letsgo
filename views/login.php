f0b59a5101b9b42a07edf5712377d6495043d2e4
Derek Jobst <derekjobst@gmail.com>
2013-10-19 21:47:55 -0400

    Renamed Main

--- views/login.php
+++ views/login.php
@@ -1,3 +1,6 @@
+<link href="style-login.css" rel="stylesheet" type="text/css">
+
+
 <div id="loginform">
 	<form action="" method="POST">
 		<label for="username"></label><input id="username" type="text" name="username" placeholder="Username"><br />
