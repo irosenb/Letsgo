f283b59c99cb100f9a3b002dce4b42332781f37a
Derek Jobst <derekjobst@gmail.com>
2013-10-20 11:32:03 -0400

    Login Form Edits

--- views/login.php
+++ views/login.php
@@ -1,9 +1,11 @@
 <link href="/resources/style-login.css" rel="stylesheet" type="text/css">
 
-<div id="loginform">
-	<form action="/login" method="POST">
-		<input class="login-field" id="email" type="text" name="email" placeholder="Email address"><br />
-		<input class="login-field" id="password" type="password" name="password" placeholder="Password"><br />
-		<input type="submit" class="button login-button" value="Login"><br/>
-	</form>
+<div class="login-wrapper">
+	<div id="loginform">
+		<form action="/login" method="POST">
+			<input class="login-field" id="email" type="text" name="email" placeholder="Email address"><br />
+			<input class="login-field" id="password" type="password" name="password" placeholder="Password"><br />
+			<input type="submit" class="button login-button" value="Login"><br/>
+		</form>
+	</div>
 </div>
