5a48cdf7622d31f940f401cab5a6842be3135350
Derek Jobst <derekjobst@gmail.com>
2013-10-19 23:20:59 -0400

    Login Stuff

--- views/login.php
+++ views/login.php
@@ -8,10 +8,9 @@
 
 
 <div id="loginform">
-	<form action="" method="POST">
-		<label for="username"></label><input id="username" type="text" name="username" placeholder="Username"><br />
-		<label for="password"></label><input id="password" type="password" name="password" placeholder="Password"><br />
-		<input type="submit" value="Login"><br/>
-	</form>
-</div>
-</html>
+		<form action="" method="POST">
+			<label for="username"></label><input class="login-field" id="username" type="text" name="username" placeholder="Username"><br />
+			<label for="password"></label><input class="login-field" id="password" type="password" name="password" placeholder="Password"><br />
+			<input type="submit" class="button login-button" value="Login"><br/>
+		</form>
+	</div>
